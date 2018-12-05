783fa85840dd4228bbcd6a7bd919fa2f002e39c8
Pär Strandberg <par.strandberg@gmail.com>
2018-12-05 23:18:56 +0100

    timer: leading zero time renders on timer component. pause/play.

--- src/timer/Timer.jsx
+++ src/timer/Timer.jsx
@@ -4,19 +4,24 @@ import {startTimer, toggleTimer} from "./timerActions"
 import {bindActionCreators} from "redux";
 
 class Timer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            minutes: 10,
-            seconds: 0,
-        };
-    }
+    addLeadingZeroToTime = (time) => {
+        return time.toString().length === 1 ? `0${time}` : time;
+    };
+
+    parseSecondsToTime = (seconds) => {
+        const parsedMinutes = this.addLeadingZeroToTime(Math.floor(seconds / 60));
+        const parsedSeconds = this.addLeadingZeroToTime(seconds - parsedMinutes * 60);
+
+        return (
+            <h2>{parsedMinutes}:{parsedSeconds}</h2>
+        );
+    };
 
     render() {
         const {timer} = this.props;
         return (
             <div>
-                <h1>{this.props.minutes}:{this.props.seconds}</h1>
+                {this.parseSecondsToTime(timer.time)}
                 {timer.active ?
                     <button onClick={this.props.toggleTimer}>Pause timer</button> :
                     <button onClick={this.props.startTimer}>Start timer</button>
