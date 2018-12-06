445c0d4d4dacda93585a5546b48a51f343ffa8bd
Pär Strandberg <par.strandberg@gmail.com>
2018-12-06 20:59:06 +0100

    users: more random user images.

--- src/user/userImages.js
+++ src/user/userImages.js
@@ -1,3 +1,17 @@
 export const userImages = [
+    // male famous
     "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/12/11/naturo-monkey-selfie.jpg",
+    "http://thinkup.me/wp-content/uploads/2017/11/Jim_Carrey-1-e1510001639348.jpg",
+    "https://cdn.onlyinyourstate.com/wp-content/uploads/2016/07/3630750300_f1cd14cdc3_b-700x875.jpg",
+    "https://financialpostcom.files.wordpress.com/2015/01/nicolas-cage.jpg",
+    "https://www.rd.com/wp-content/uploads/2014/04/12-famous-introverts-elton-john-sl.jpg",
+    "https://img.buzzfeed.com/buzzfeed-static/static/2014-03/campaign_images/webdr05/4/20/71-famous-people-who-were-total-frat-boys-at-one--1-19771-1393983351-2_big.jpg",
+
+    // animals
+    "https://www.drusillas.co.uk/images/whats-on-card/redpanda-profile-400x400-984.jpg",
+    "http://mymodernmet.com/wp/wp-content/uploads/2017/01/animal-selfies-thumbnail.jpg",
+    "https://news.nationalgeographic.com/content/dam/news/2016/08/20/WAQ-big-eyes/01WAQbigeyes.ngsversion.1471633019887.adapt.1900.1.jpg",
+    "https://www.wpcc.org.uk/images/nature/mammals/header-squirrel.jpg",
+    "https://www.gvi.co.uk/wp-content/uploads/2016/05/2-1.png",
+    "https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/29/2015/01/iStock_000005542321_Medium.jpg",
 ];
