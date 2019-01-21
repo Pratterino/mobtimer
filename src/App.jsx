e030d85c3f41e48882d7e1379134b9a7e9485776
Pär Strandberg <par.strandberg@gmail.com>
2019-01-21 20:44:04 +0100

    css notification messages moved.

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ class App extends Component {
                 </div>
                 }
 
-                <Settings/>
-
                 <Notifications/>
+
+                <Settings/>
 
                 <Users/>
 
