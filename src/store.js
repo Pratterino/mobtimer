30a1b05338e33023321423dae58b20015e988bb8
Pär Strandberg <par.strandberg@gmail.com>
2019-01-23 20:32:40 +0100

    redux dev tools

--- src/store.js
+++ src/store.js
@@ -14,5 +14,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer);
+export const store = createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export const persistor = persistStore(store);
